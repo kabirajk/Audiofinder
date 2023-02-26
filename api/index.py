ff89abaeac3095dd9ccaa2e951bc422cd944433d
kabirajk <kabiraj.mc19@bitsathy.ac.in>
2023-02-26 16:29:09 +0530

    code check

--- api/index.py
+++ api/index.py
@@ -1,6 +1,4 @@
 from flask import Flask, request , jsonify
-import os , sys
-
 
 app = Flask(__name__)
 
@@ -9,10 +7,10 @@ global count
 globalImage=dict()
 count=1
 
-@app.route('/')
-def home():
-    print(globalImage);
-    return 'Hello, World!'
+# @app.route('/')
+# def home():
+#     print(globalImage);
+#     return 'Hello, World!'
 
 @app.route('/image/<id>')
 def getimage(id):
@@ -31,27 +29,18 @@ def predict():
     return jsonify({"imageId":"image/"+str(count)})
 
 @app.route('/flush')
-def home():
+def flush():
     global globalImage
     globalImage=dict()
 
-@app.route('/test' , methods=['GET','POST'])
-def test():
-	print("log: got at test" , file=sys.stderr)
-	return jsonify({'status':'succces'})
 
 @app.after_request
 def after_request(response):
-    print("log: setting cors" ,os.curdir, file = sys.stderr)
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     return response
 
-
-@app.route('/about')
-def about():
-    return 'About'
 
 # if __name__ == '__main__':
 # 	app.run(host="0.0.0.0", port=int("5000"), debug=True)
