13117bd260c82597fd520ecc81c831a8c32a2006
kabirajk <kabiraj.mc19@bitsathy.ac.in>
2023-02-26 16:22:19 +0530

    code check

--- api/index.py
+++ api/index.py
@@ -1,10 +1,6 @@
-from flask import Flask, render_template , request , jsonify
-# from PIL import Image
-import os , io , sys
-# import numpy as np 
-# import base64
-# import cv2
-# from cv2 import dnn_superres
+from flask import Flask, request , jsonify
+import os , sys
+
 
 app = Flask(__name__)
 
@@ -13,25 +9,6 @@ global count
 globalImage=dict()
 count=1
 
-# def readb64(uri):
-#    encoded_data = uri.split(',')[1]
-#    nparr = np.frombuffer(base64.b64decode(encoded_data), np.uint8)
-#    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-#    return img
-
-# def Upscale(uri,scale=3):
-#     sr = dnn_superres.DnnSuperResImpl_create()
-#     image =  readb64(uri)#cv2.imread('/content/drive/MyDrive/Colab Notebooks/img upscaler/image.jpg')
-#     sr.readModel(f"EDSR_x{scale}.pb")#path for model
-#     sr.setModel("edsr", scale)
-#     result = sr.upsample(image)
-#     # cv2.imwrite("D:/Development/EDSR_Tensorflow/models/upscaled_x3.png", result)
-#     retval, buffer = cv2.imencode('.jpg', result)
-#     jpg_as_text = base64.b64encode(buffer)
-#     # print("render")
-#     # print("data:image/jpeg;base64,"+str(jpg_as_text, 'UTF-8'))
-#     return "data:image/jpeg;base64,"+str(jpg_as_text, 'UTF-8')
-    
 @app.route('/')
 def home():
     print(globalImage);
@@ -53,13 +30,10 @@ def predict():
     print(globalImage);
     return jsonify({"imageId":"image/"+str(count)})
 
-
-# @app.route('/upscale' , methods=['POST'])
-# def upsclaeImage():
-#     input_json=request.get_json(force=True)
-#     uri=input_json['imguri']
-#     scale=input_json['scale']
-#     return jsonify({"image":Upscale(uri=uri,scale=scale)});
+@app.route('/flush')
+def home():
+    global globalImage
+    globalImage=dict()
 
 @app.route('/test' , methods=['GET','POST'])
 def test():
